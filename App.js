9b8830636ef5b2d6ccf4c4ca2cc3f47e2a34364d
Fotis Tsakiris <footios76@gmail.com>
2019-08-30 15:41:56 +0300

    Screens, Fonts p. and navigation p. added

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Fonts from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Fonts.loadAsync({
+    'open-sans': require('./assets/Fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/Fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading 
+    startAsync={fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    />
+  }
+	return (
+		<View style={styles.container}>
+			<Text style={{fontFamily: 'open-sans-bold' }} >Open up App.js to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 This is a Udemy course
 [React Native - The Practical Guide](https://www.udemy.com/react-native-the-practical-guide/)
 
-Sections:
- # 6. Navigation with React Navigation [THE MEALS APP]
+# Sections:
+ ### 6. Navigation with React Navigation [THE MEALS APP]
 
 It's a small app, where you may choose your meal, add it to favorites etc.
+
+
+## Packages installed:
+### expo install expo-fonts
+### expo install react-native-gesture-handler react-native-reanimated 
