f98c2d06063ee0db503108a9df5d4adeaea61093
fotis tsakiris <footios76@gmail.com>
2020-10-26 20:01:59 +0200

    `useScreens` is deprecated, use `enableScreens`

--- App.js
+++ App.js
@@ -1,45 +1,54 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { useScreens } from 'react-native-screens';
-import * as Fonts from 'expo-font';
-import { AppLoading } from 'expo';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { enableScreens } from "react-native-screens";
+import * as Fonts from "expo-font";
+import { AppLoading } from "expo";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
-import MealsNavigator from './navigation/MealsNavigator';
-import mealsReducer from './store/reducers/meals'
+import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
 
-useScreens(); // Unlocks the native screens of each platform! Better performance.
+enableScreens(); // Unlocks the native screens of each platform! Better performance.
 
 const rootReducer = combineReducers({
-	meals: mealsReducer
-})
+  meals: mealsReducer
+});
 
 const store = createStore(rootReducer);
 
 // For loading Fonts. Returns a promise!
 const fetchFonts = () => {
-	return Fonts.loadAsync({
-		'open-sans': require('./assets/Fonts/OpenSans-Regular.ttf'),
-		'open-sans-bold': require('./assets/Fonts/OpenSans-Bold.ttf')
-	});
+  return Fonts.loadAsync({
+    "open-sans": require("./assets/Fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/Fonts/OpenSans-Bold.ttf")
+  });
 };
 
 export default function App() {
-	const [ fontLoaded, setFontLoaded ] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-	if (!fontLoaded) {
-		// Load the Fonts...
-		return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
-	}
-	return <Provider store={store}><MealsNavigator /></Provider> ;
+  if (!fontLoaded) {
+    // Load the Fonts...
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
